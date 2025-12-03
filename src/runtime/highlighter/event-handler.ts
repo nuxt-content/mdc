0ceddebc8d82dc8e36c5880016fc4858d3b7f425
Farnabaz <farnabaz@gmail.com>
2025-12-03 14:48:20 +0100

    fix: add cache on highlight event handler

--- src/runtime/highlighter/event-handler.ts
+++ src/runtime/highlighter/event-handler.ts
@@ -1,9 +1,13 @@
-import { eventHandler, getQuery } from 'h3'
+import { getQuery } from 'h3'
+import { cachedEventHandler } from '#imports'
 
-export default eventHandler(async (event) => {
+export default cachedEventHandler(async (event) => {
   const { code, lang, theme: themeString, options: optionsStr } = getQuery(event)
   const theme = JSON.parse(themeString as string)
   const options = optionsStr ? JSON.parse(optionsStr as string) : {}
   const highlighter = await import('#mdc-highlighter').then(m => m.default)
   return await highlighter(code, lang, theme, options)
+}, {
+  maxAge: 60 * 60 * 24 * 30,
+  swr: true
 })
