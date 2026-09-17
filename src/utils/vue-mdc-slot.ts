4efdaf49f06a046bb444f16eacfe0d6cdfed9353
lfx <dostufffancy@qq.com>
2026-09-17 17:55:05 +0800

    fix: respect user-defined nodeTransforms from nuxt.config

--- src/utils/vue-mdc-slot.ts
+++ src/utils/vue-mdc-slot.ts
@@ -6,6 +6,7 @@ export const registerMDCSlotTransformer = (resolver: Resolver) => {
   extendViteConfig((config) => {
     const compilerOptions = (config as any).vue.template.compilerOptions
     compilerOptions.nodeTransforms = [
+      ...compilerOptions.nodeTransforms ?? [],
       <NodeTransform> function viteMDCSlot(node: ElementNode, context) {
         const isVueSlotWithUnwrap = node.tag === 'slot' && node.props.find(p => p.name === 'mdc-unwrap' || p.name === 'mdcUnwrap' || (p.name === 'bind' && (p as DirectiveNode).rawName === ':mdc-unwrap'))
         const isMDCSlot = node.tag === 'MDCSlot'
